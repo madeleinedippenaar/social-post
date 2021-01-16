083983709963803eedc15c2a6bc4b9706cd1744f
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-01-16 16:25:50 -0500

    project created and components made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocialPostsComponent } from './shared/components/social-posts/social-posts.component';
+import { PostComponent } from './shared/components/post/post.component';
+import { PostFormComponent } from './shared/components/post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialPostsComponent,
+    PostComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/components/social-posts/social-posts.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../../models/social';
+
+@Component({
+  selector: 'app-social-posts',
+  templateUrl: './social-posts.component.html',
+  styleUrls: ['./social-posts.component.css']
+})
+export class SocialPostsComponent implements OnInit {
+
+  posts: Post[] = [
+    {title: 'test', thoughts: 'test'}
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSumbit(post: Post) {
+    this.posts. push(post);
+  }
+
+  onDelete(post: Post) {
+
+  }
+
+}
