7b291e3127030beeefbb970036299b65cf1a8e2d
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-01-20 13:30:24 -0500

    structural changes

--- src/app/shared/components/social-posts/social-posts.component.ts
+++ src/app/shared/components/social-posts/social-posts.component.ts
@@ -9,7 +9,8 @@ import { Post } from '../../models/social';
 export class SocialPostsComponent implements OnInit {
 
   posts: Post[] = [
-    {title: "test", thoughts: "test thoughts"}
+    {title: "Angular is Great", thoughts: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut tortor pretium viverra suspendisse potenti nullam ac tortor. Tincidunt dui ut ornare lectus sit amet est placerat in."},
+    {title: "Coding is so Fun!", thoughts: "Nisi est sit amet facilisis magna. Sit amet mattis vulputate enim. Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Cras adipiscing enim eu turpis egestas pretium aenean pharetra magna. Justo donec enim diam vulputate ut pharetra. Elit sed vulputate mi sit amet mauris commodo quis imperdiet. Diam phasellus vestibulum lorem sed risus ultricies."}
   ];
 
   showPostForm: boolean = false;
